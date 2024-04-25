64e982ac7c8ec677a6a36ed096c4e13c4ff38745
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2024-04-25 11:00:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function mincost(arr)
 { 
 //write your code here
+	    arr.sort((a, b) => a - b);
+	let minCost =0
+	for (let i = i<arr.length; i++)	
+		  minCost += arr[i];
+	}
 // return the min cost
+    return minCost;
+
   
 }
 
